0e45df43fb7395cf694b348b16263b7b4798c83f
MrBurp <andreass.ziegler@gmail.com>
2023-01-21 11:58:36 +0100

    create connect back and with frontend

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -1,11 +1,15 @@
+import { Product } from "./product.model";
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class ProductService {
+  baseUrl = "http://localhost:3001/products";
+
   constructor(private snackbar: MatSnackBar, private http: HttpClient) {}
 
   showMessage(msg: string): void {
@@ -14,5 +18,8 @@ export class ProductService {
       horizontalPosition: "right",
       verticalPosition: "top",
     });
+  }
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product);
   }
 }
