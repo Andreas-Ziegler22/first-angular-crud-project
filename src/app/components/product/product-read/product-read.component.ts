73bd496a35ac53f9e8e3a4eb19393b3a5fed81cc
MrBurp <andreass.ziegler@gmail.com>
2023-01-26 11:44:51 +0100

    it was missing s coma

--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./product-read.component.css"],
 })
 export class ProductReadComponent implements OnInit {
-  products: Product[] | undefined;
+  products!: Product[];
   displayedColumns = ["id", "name", "price"];
 
   constructor(private productService: ProductService) {}
