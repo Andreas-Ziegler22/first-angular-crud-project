ee8e9da22c5cf6ed8327581e27b1e82bd071c770
MrBurp <andreass.ziegler@gmail.com>
2023-01-31 17:35:16 +0100

    delete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,7 @@ import { MatSortModule } from "@angular/material/sort";
 import localeDe from "@angular/common/locales/de";
 import { registerLocaleData } from "@angular/common";
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 registerLocaleData(localeDe);
 
@@ -53,6 +54,7 @@ registerLocaleData(localeDe);
     ProductReadComponent,
     ProductRead2Component,
     ProductUpdateComponent,
+    ProductDeleteComponent,
   ],
   imports: [
     BrowserModule,
