f99061a7f7fe933c85fe4810fc94cdb27cf5eead
MrBurp <andreass.ziegler@gmail.com>
2023-02-04 12:58:17 +0100

    change data with  BehaviorSubject

--- src/app/components/template/header/header.service.ts
+++ src/app/components/template/header/header.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HeaderData } from "./header-data.model";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class HeaderService {
+  HeaderData = new BehaviorSubject<HeaderData>({
+    title: "Home",
+    icon: "home",
+    routeUrl: "",
+  });
 
-  constructor() { }
+  constructor() {}
 }
